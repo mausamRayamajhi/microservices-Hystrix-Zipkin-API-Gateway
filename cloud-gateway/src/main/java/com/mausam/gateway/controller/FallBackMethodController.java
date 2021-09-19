0a447338388754fec87b37d5adf700e1510429c4
Mausam Rayamajhi <mausam495@gmail.com>
2021-09-20 09:27:00 +1000

    fallback method has been added

---
+++ cloud-gateway/src/main/java/com/mausam/gateway/controller/FallBackMethodController.java
@@ -0,0 +1,20 @@
+package com.mausam.gateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackMethodController {
+
+    @GetMapping("userServiceFallBack")
+    public String userServiceFallBackMethod(){
+        return "User Service is taking longer time than Expected." +
+                " Please try Again later";
+    }
+
+    @GetMapping("departmentServiceFallBack")
+    public String departmentServiceFallBackMethod(){
+        return "Department Service is taking longer time than Expected." +
+                " Please try Again later";
+    }
+}
