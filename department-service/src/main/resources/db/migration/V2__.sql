fa67b45bbb469255696220d59668de5e3b7139d1
Mausam Rayamajhi <mausam.rayamajhi@tabcorp.com.au>
2023-05-08 00:59:11 +1000

    id changed

--- department-service/src/main/resources/db/migration/V2__.sql
+++ department-service/src/main/resources/db/migration/V2__.sql
@@ -1,4 +1,4 @@
 INSERT INTO department (department_id, department_name, department_address, department_code)
-VALUES (101, "COMPUTER", "KATHMANDU", "KTM101");
+VALUES (1, "COMPUTER", "KATHMANDU", "KTM101");
 INSERT INTO department (department_id, department_name, department_address, department_code)
-VALUES (102, "SCIENCE", "BIRATNAGAR", "BIR101");
+VALUES (2, "SCIENCE", "BIRATNAGAR", "BIR101");
